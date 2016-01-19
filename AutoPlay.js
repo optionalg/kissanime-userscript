a594f9d956e367b090104624d268b3c5d74de3b9
mattd360 <mattmarillac@hotmail.com>
2016-01-20 02:13:28 +1000

    first commit

---
+++ AutoPlay.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        KissAnime Auto Play Next Episode
+// @namespace   matthewmarillac.com
+// @include     *://kissanime.com/*
+// @include     *://kisscartoon.me/*
+// @include     *://kissanime.to/*
+// @include     *://kissasian.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+var videoPlaceholder = document.getElementById('divContentVideo');
+var video = videoPlaceholder.getElementsByTagName('video')[0];
+ 
+video.addEventListener('ended', myHandler, false);
+function myHandler(e) {
+  var element = document.getElementById('btnNext').parentNode;
+  window.location = element.href;
+}
