793e14ded5d02a160f43b26c1a19067afee3d5f0
mattd360 <mattmarillac@hotmail.com>
2016-01-24 02:38:10 +1000

    added backend resources

---
+++ Backend/meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        KissAnime Auto Play Next Episode
+// @author      Matthew James de Marillac
+// @namespace   matthewmarillac.com
+// @description Instantly plays the next episode in the anime list without exiting fullscreen mode.
+// @include     *://kissanime.com/*
+// @include     *://kisscartoon.me/*
+// @include     *://kissanime.to/*
+// @include     *://kissasian.com/*
+// @downloadURL   http://matthewmarillac.com/api/AutoPlay.js
+// @version     5
+// @grant       none
+// ==/UserScript==
