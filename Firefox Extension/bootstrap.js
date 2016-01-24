63045d9fb02d68809389cb7e6fdf0c093a2a1e04
mattd360 <mattmarillac@hotmail.com>
2016-01-25 09:29:25 +1000

    firefox extension finished

--- Firefox Extension/bootstrap.js
+++ Firefox Extension/bootstrap.js
@@ -1,3 +1,6 @@
-        chrome.tabs.executeScript(null, {"file": "AutoPlay.js"});
-
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+     if (tab.url && tab.url.indexOf('https://kissanime.to/') === 0 || tab.url && tab.url.indexOf('https://kisscartoon.me/') === 0 || tab.url && tab.url.indexOf('https://kissasian.com/') === 0 ) {       
+       chrome.tabs.executeScript(null, {"file": "AutoPlay.js"});
+     }
+});
 
