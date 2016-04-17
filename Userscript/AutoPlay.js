3d557bad87d58b03e390b52b500245a8aa36548d
Matt de Marillac <mattdemarillac@Matts-Air.gateway>
2016-04-17 19:03:25 +1000

    fixed firefox overlay issue

--- Userscript/AutoPlay.js
+++ Userscript/AutoPlay.js
@@ -227,7 +227,7 @@ function getTime(totalSec)
 function createOverlay()
 {
 createButton();
-$(videoPlaceholder).append("<div id='overlay'></div>");
+$(".video-js").prepend("<div id='overlay'></div>");
  
   $("body").append("<style>#overlay {position: absolute; right:0; bottom: 35px; color: #FFF; text-align: center; font-size: 20px; background-color: rgba(7, 20, 30, 0.7); width: 640px; padding: 10px 0; z-index: 2147483647; border: 2px solid rgba(128, 128, 128, 0.35);}</style>");
   editMessage("<div><p>Thanks for using Kissanime Autoplayer. Be sure to leave a rating if you enjoy using it!</p>"+
