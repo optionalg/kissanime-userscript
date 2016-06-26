2a7e1e6136676c1081affe1763d89a125924ffbe
Matthew de Marillac <mattmarillac@Hotmail.com>
2016-06-26 16:31:29 +1000

    added anilist api

---
+++ Userscript/anilist/main.js
@@ -0,0 +1,25 @@
+var nani = require('nani').init('mattmarillac-cfvnh', '');
+
+window.getAnime = function(url){
+
+nani.get('anime/'+url)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+}
+
+window.searchAnime = function(url){
+
+nani.get('anime/search/'+url)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+}
